43ca29085c5f162d4c3099d54069f06219eed268
Chris Turner <cturner914@gmail.com>
2017-09-16 15:21:48 -0400

    Initial commit with project structure along with Twitter and AFNetworking pods.

---
+++ SeeClickFixCodeTest/SeeClickFixCodeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SeeClickFixCodeTest
+//
+//  Created by Chris Turner on 9/16/17.
+//  Copyright © 2017 Chris Turner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
